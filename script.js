d9188c2138ffe58d8cbeca35611e79832fef6ff0
sasha-go <sasha.lfg@gmail.com>
2020-01-16 22:05:49 -0800

    added params to getRestuarants function, Math.random on restaurant results

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const zomatoApiKey = "bdf061b7ff13160c0b5ed3be06170ae7";
 // Zomato API
 const zomatoUrl = "https://developers.zomato.com/api/v2.1";
 
-
 // Function to generate entity_id for the user's city using /locations? endpoint
 function getEntityID(userCity) {
     const options = {
@@ -55,6 +54,9 @@ function getRestaurants(entity_id) {
     const params = {
         entity_id: entity_id,
         entity_type: "city",
+        count: 15,
+        sort: "rating",
+        order: "desc"
     };
 
     let queryString = $.param(params);
@@ -85,24 +87,38 @@ function getRestaurants(entity_id) {
 // Display restaurants using response from restaurants for a user's given city 
 // !!! Need to create field for budget and add it to conditional statement below
 // Need to figure out how to pull more than the 20 restaurants shown 
+
+// Figure out mathrandom
+// figure out cuisine 
+
 function displayRestaurants(responseJson) {
     $('#results').empty();
     const budget = $('#js-budget').val();
-    for (let i = 0; i < responseJson.restaurants.length; i++) {
-        if (responseJson.restaurants[i].restaurant.average_cost_for_two <= budget) {
+    let randomNum = Math.floor(Math.random(responseJson.restaurants.length) * 5) + 1;
+    console.log(randomNum);
+
+    for (let i = randomNum; i < responseJson.restaurants.length; i++) {
+        let  restaurantsJson = responseJson.restaurants[i].restaurant;
+        if (restaurantsJson.average_cost_for_two <= budget && restaurantsJson.user_rating.aggregate_rating > 0) {
         $('#results').append(`<li>
-        <img src="${responseJson.restaurants[i].restaurant.featured_image}" alt="featured image for restaurant result" id="featuredImg">
-        <a href='${responseJson.restaurants[i].restaurant.url}' target="_blank">${responseJson.restaurants[i].restaurant.name}</a> 
-        | <a href='${responseJson.restaurants[i].restaurant.menu_url}' target="_blank">Menu</a>
-        <p>Average Cost for 2 $${responseJson.restaurants[i].restaurant.average_cost_for_two}</p>
-        <br>
-        <br>
+        <img src="${restaurantsJson.featured_image}" id="featuredImg" alt="feature image for ${restaurantsJson.name}" onerror="imgError(this);"/>
+        <a href='${restaurantsJson.url}' target="_blank">${restaurantsJson.name}</a> 
+        | <a href='${restaurantsJson.menu_url}' target="_blank">Menu</a>
+        <p>${restaurantsJson.location.address}</p>
+        <p>${restaurantsJson.location.locality_verbose}</p>
+        <p>Average Cost for 2 $${restaurantsJson.average_cost_for_two}</p>
+        <p>${restaurantsJson.user_rating.aggregate_rating}</p>
         </li>`)
         }
     };
     $('#results').removeClass("hidden");
 }
 
+function imgError(image) {
+    image.onerror = "";
+    image.src = "/images/noimage.gif";
+    return true;
+}
 
 function watchForm() {
     $('form').submit(event => {
